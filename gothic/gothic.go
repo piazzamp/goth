58f33637cdc1bf338da764424a875c621f98e9a6
Matt Piazza <piazzamp@g.cofc.edu>
2016-03-30 22:00:09 -0400

    boolean algebra mods

--- gothic/gothic.go
+++ gothic/gothic.go
@@ -132,7 +132,7 @@ See https://github.com/piazzamp/goth/examples/main.go to see this in action.
 */
 var CompleteUserAuth = func(res http.ResponseWriter, req *http.Request) (goth.User, error) {
 
-	if !keySet && defaultStore == Store {
+	if !keySet || defaultStore == Store {
 		fmt.Println("goth/gothic: no SESSION_SECRET environment variable is set. The default cookie store is not available and any calls will fail. Ignore this warning if you are using a different store.")
 	}
 
